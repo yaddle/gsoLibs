6343b83c70ad9d5139b0ee9ccd1aacc7590833b5
gamsteron <gamsteron@gmail.com>
2018-04-10 22:13:19 +0200

    pre

--- LibLoader.lua
+++ LibLoader.lua
@@ -27,8 +27,10 @@ class "__gsoLibLoader"
                 _G.gsoSDK.Orbwalker = __gsoOrbwalker()
                 -----------------------------------------------------------
                 -- MENU
-                self.selmenu = MenuElement({name = "Orbwalker & Prediction Selection", id = "gsoorbsel", type = MENU, leftIcon = "https://raw.githubusercontent.com/gamsteron/GoSExt/master/Icons/seliconjs7sdq.png" })
+                self.selmenu = MenuElement({name = "Orbwalker & Prediction & TS", id = "gsoorbsel", type = MENU, leftIcon = "https://raw.githubusercontent.com/gamsteron/GoSExt/master/Icons/seliconjs7sdq.png" })
                 self.selmenu:MenuElement({ id = "orbsel", name = "Orbwalker", value = 1, drop = { "Gamsteron", "GOS", "IC" } })
+                self.selmenu:MenuElement({ id = "predsel", name = "Prediction", value = 1, drop = { "Noddy + GOS", "TPred", "HPred", "EPred" } })
+                self.selmenu:MenuElement({ id = "tssel", name = "Target Selector", value = 1, drop = { "Gamsteron", "IC" } })
                 _G.gsoSDK.TS:CreateMenu(menu)
                 _G.gsoSDK.Orbwalker:CreateMenu(menu, self.selmenu)
                 menu:MenuElement({name = "Drawings", id = "gsodraw", leftIcon = "https://raw.githubusercontent.com/gamsteron/GoSExt/master/Icons/circles.png", type = MENU })
