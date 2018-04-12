5f5242a2fd77d6d2f147c9df9694cc83a1faea6f
gamsteron <gamsteron@gmail.com>
2018-04-12 18:51:11 +0200

    const class

--- gsoSDK.lua
+++ gsoSDK.lua
@@ -14,9 +14,7 @@ class "__gsoSDK"
         function __gsoSDK:__init(menu)
                 require "gsoLibs\\AutoUpdate"
                 _G.gsoSDK.AutoUpdate = __gsoAutoUpdate()
-                if  _G.gsoSDK.AutoUpdate:CanUpdate(0.01, "https://raw.githubusercontent.com/gamsteron/gsoLibs/master/version/LibLoader.version") then
-                        _G.gsoSDK.AutoUpdate:Update(COMMON_PATH .. "gsoLibs\\LibLoader.lua", "https://raw.githubusercontent.com/gamsteron/gsoLibs/master/LibLoader.lua")
-                end
+                _G.gsoSDK.AutoUpdate:Update(COMMON_PATH .. "gsoLibs\\LibLoader.lua", "https://raw.githubusercontent.com/gamsteron/gsoLibs/master/LibLoader.lua")
                 require "gsoLibs\\LibLoader"
                 self.Loader = __gsoLibLoader(menu)
         end
